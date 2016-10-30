1e9413d46ac2e6f998795cf9ed4b42e9907d62ea
Sankari <8eikochan@gmail.com>
2016-10-30 14:49:41 +0200

    Start building Game class

--- src/com/ebingine/Game.java
+++ src/com/ebingine/Game.java
@@ -9,6 +9,50 @@ package com.ebingine;
  * @version 2016.1025
  * @since 1.7
  */
-abstract public class Game {
+public class Game implements Runnable{
+
+    private Thread thread;
+    private boolean running = false;
+
+    public Game() {
+
+    }
+
+    public void start() {
+        // If game is running, doesn't start it again
+        if (running)
+            return;
+
+        // todo initialize engine components
+
+        // Game implements Runnable so it can be passed to Thread
+        thread = new Thread(this);
+        // Starts the game loop
+        thread.run();
+
+    }
+
+    public void run() {
+        //
+        double firstTime = System.nanoTime() / 1000000000.0;
+
+        // While the game loop is running
+        while (running) {
+
+        }
+    }
+
+    public void stop() {
+        // No need to stop if it's already stopped
+        if (!running)
+            return;
+
+        // Stops the game loop
+        running = false;
+    }
+
+    public void clear() {
+
+    }
 
 }
